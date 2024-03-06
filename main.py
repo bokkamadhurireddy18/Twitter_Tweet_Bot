810084e5a07d71d7ec846a0b50e58f9743235538
madhurireddybokka18 <madhurireddybokka1811@gmail.com>
2024-03-06 14:38:32 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+# Create and configure the Chrome webdriver
+driver = webdriver.Chrome(options=chrome_options)
+
+TWITTER_EMAIL = "example@gmail.com"
+TWITTER_PASSWORD = "xyz"
+
+PROMISED_DOWN= '1000'
+PROMISED_UP='200'
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.driver = driver
+        self.down = 0
+        self.up = 0
+
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+
+        go_button = self.driver.find_element(By.XPATH,'//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+        go_button.click()
+
+        time.sleep(60)
+        self.download_mbps= self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span')
+        #print(self.download_mbps.text)
+        self.upload_mbps= self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
+        #print(self.upload_mbps.text)
+
+    def tweet_at_provider(self):
+        self.driver.get("https://twitter.com/login")
+
+        time.sleep(2)
+        email = self.driver.find_element(By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
+        password = self.driver.find_element(By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+
+        email.send_keys(TWITTER_EMAIL)
+        password.send_keys(TWITTER_PASSWORD)
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
+
+        time.sleep(5)
+        tweet_compose = self.driver.find_element(By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+
+        tweet = f"Hey Internet Provider, why is my internet speed {self.download_mbps}down/{self.upload_mbps}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+
+        tweet_button = self.driver.find_element(By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button.click()
+
+        time.sleep(2)
+        self.driver.quit()
+
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+bot.tweet_at_provider()
